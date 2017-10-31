a117d4533c0b2c116a1ea3dc7a6137cfe1f6619d
agnes-kabaly <alicet.woods@gmail.com>
2017-10-31 22:35:23 +0100

    remove, main

--- src/GenericArrayList.java
+++ src/GenericArrayList.java
@@ -16,11 +16,25 @@ public class GenericArrayList<Type> {
     }
 
     public Type get(int index) {
+        if (index >= typeArray.length) {
+            throw new RuntimeException("Invalid index");
+        }
         return typeArray[index];
     }
 
     public Integer size() {
         return typeArray.length;
+    }
+
+    public void remove(int index) {
+        Type[] newTypeArray = (Type[]) new Object[typeArray.length-1];
+        for (int i = 0; i < index; i++) {
+            newTypeArray[i] = typeArray[i];
+        }
+        for (int i = index+1; i < typeArray.length; i++) {
+            newTypeArray[i-1] = typeArray[i];
+        }
+        typeArray = newTypeArray;
     }
 
 
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,25 @@
 public class Test {
+
+    public static void main(String[] args) {
+        GenericArrayList<Integer> testList = new GenericArrayList<>();
+
+        testList.add(4);
+        testList.add(88);
+        testList.add(99);
+        testList.add(120);
+        testList.add(1600);
+
+        System.out.println("Size: " + testList.size());
+        System.out.println("Get 0.: " + testList.get(0));
+        System.out.println("Get 4.: " + testList.get(4));
+
+        testList.add(8800);
+        System.out.println("Size02.: " + testList.size());
+        System.out.println("Get 5.: " + testList.get(5));
+
+        //testList.remove(4);
+
+
+    }
+
 }
